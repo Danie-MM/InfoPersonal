33a167608e2b4bfb4eb3302bf6a0818159d5fa10
Daniela Morales <dmoralesme@ucenfotec.ac.cr>
2025-01-30 16:50:26 -0600

    Add files via upload
    Subiendo mi proyecto inicial a GitHub

---
+++ script.js
@@ -0,0 +1,26 @@
+emailjs.init('service_9ghq4bl');  
+
+
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault(); 
+
+    const nombre = document.getElementById('nombre').value;
+    const email = document.getElementById('email').value;
+    const mensaje = document.getElementById('mensaje').value;
+
+
+    emailjs.send('service_9ghq4bl', 'template_v20t8o8', {
+        from_name: nombre,
+        from_email: email,
+        message: mensaje
+    })
+    .then(function(response) {
+        console.log('Success!', response);
+        alert('Tu mensaje ha sido enviado con éxito!');
+        
+        document.getElementById('contact-form').reset();
+    }, function(error) {
+        console.log('Failed...', error);
+        alert('Algo salió mal, intenta nuevamente.');
+    });
+});
